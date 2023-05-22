3cc7f3bd5449dfa0af557bca0f8ea9b3460f95b5
alimarchi <marchi_alice@yahoo.com>
2023-05-22 11:12:17 +0200

    scroll to top

--- src/components/Arrow.js
+++ src/components/Arrow.js
@@ -1,14 +1,37 @@
 import "../style/Arrow.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
 
 const Arrow = () => {
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    const handleScrollButtonVisibility = () => {
+      window.pageYOffset > 300 ? setShowButton(true) : setShowButton(false);
+    };
+
+    window.addEventListener("scroll", handleScrollButtonVisibility);
+
+    return () => {
+      window.removeEventListener("scroll", handleScrollButtonVisibility);
+    };
+  }, []);
+
+  const handleScrollTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
-    <div className="container">
-      <div className="arrow-container">
-        <FontAwesomeIcon icon={faArrowUp} size="2x" />
-      </div>
-    </div>
+    <>
+      {showButton && (
+        <div className="container">
+          <div className="arrow-container" onClick={handleScrollTop}>
+            <FontAwesomeIcon icon={faArrowUp} size="2x" />
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
