7ec59df67ca48bb08c05adf1d49e1e618079b6b3
alimarchi <marchi_alice@yahoo.com>
2023-06-08 15:33:54 +0200

    add notes project

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,13 +2,14 @@ import wordleImg from "../assets/wordle.png";
 import shoppingCartImg from "../assets/shopping-cart.png";
 import weatherAppImg from "../assets/weather-app.png";
 import watchlistImg from "../assets/watchlist.png";
+import notesImg from "../assets/my-notes-app.png";
 
 export const projects = [
   {
     id: 1,
     name: "Wordle",
     type: "Personal project",
-    tech: " React - JavaScript - CSS",
+    tech: "React - JavaScript - CSS",
     img: wordleImg,
     repoURL: "https://github.com/alimarchi/wordle",
     webURL: "https://alimarchi.github.io/wordle/",
@@ -17,7 +18,7 @@ export const projects = [
     id: 2,
     name: "Shopping Cart",
     type: "Personal project",
-    tech: " React - JavaScript - CSS",
+    tech: "React - JavaScript - CSS",
     img: shoppingCartImg,
     repoURL: "https://github.com/alimarchi/shopping-cart",
     webURL: "https://alimarchi.github.io/shopping-cart/",
@@ -26,7 +27,7 @@ export const projects = [
     id: 3,
     name: "Weather App",
     type: "Personal project",
-    tech: " React - JavaScript - CSS",
+    tech: "React - JavaScript - CSS",
     img: weatherAppImg,
     repoURL: "https://github.com/alimarchi/react-weather-app",
     webURL: "https://alimarchi.github.io/react-weather-app/",
@@ -35,9 +36,18 @@ export const projects = [
     id: 4,
     name: "Movies Watchlist",
     type: "Personal project",
-    tech: " React - JavaScript - CSS",
+    tech: "React - JavaScript - CSS",
     img: watchlistImg,
     repoURL: "https://github.com/alimarchi/movies-watchlist",
     webURL: "https://alimarchi.github.io/movies-watchlist/",
+  },
+  {
+    id: 5,
+    name: "My Notes App",
+    type: "Personal project",
+    tech: "Typescript - React - Node.js",
+    img: notesImg,
+    repoURL: "https://github.com/alimarchi/notes-app",
+    webURL: "https://github.com/alimarchi/notes-app",
   },
 ];
